462e0453ae263f41bee51323aa684ba2081f25f4
DoYourLove <xyf15620168540@163.com>
2020-09-18 10:29:19 +0800

    [XiaoYiFan] feat : problem 3 use postConstruct

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,16 +1,21 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Bar {
 
     private Foo foo;
 
-    @Autowired
-    public void setFoo(Foo foo) {
+    public Bar(Foo foo) {
         this.foo = foo;
+    }
+
+    @PostConstruct
+    public void init() {
+        foo.setBar(this);
     }
 
     public void hi() {
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,6 +1,5 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,7 +7,6 @@ public class Foo {
 
     private Bar bar;
 
-    @Autowired
     public void setBar(Bar bar) {
         this.bar = bar;
     }
