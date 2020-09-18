ddbd9d8157f61798de2713feaf7e581465986247
DoYourLove <xyf15620168540@163.com>
2020-09-18 10:22:59 +0800

    [XiaoYiFan] feat : problem 2 use getBean

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -1,22 +1,34 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Lookup;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class GreetingController {
+public class GreetingController implements ApplicationContextAware {
 
-    private final GreetingService greetingService;
+    private GreetingService greetingService;
+
+    private ApplicationContext context;
 
     @Autowired
     public GreetingController(GreetingService greetingService) {
+        System.out.println("++GreetingController init++");
         this.greetingService = greetingService;
     }
 
     @GetMapping("/greet")
     public String greet() {
+        this.greetingService = context.getBean(GreetingService.class);
         return greetingService.sayHi();
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        context = applicationContext;
+    }
 }
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,9 +1,17 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.beans.factory.config.ConfigurableBeanFactory.SCOPE_PROTOTYPE;
+
 @Service
+@Scope(value = SCOPE_PROTOTYPE)
 public class GreetingService {
+
+    public GreetingService() {
+        System.out.println("++GreetingService init++");
+    }
 
     String sayHi() {
         return "hello world";
